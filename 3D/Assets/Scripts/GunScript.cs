0643c8ed5db8aea060292211270e7e88175ef867
Alvaro Padilla <monster_what@hotmail.com>
2023-03-13 19:57:26 -0600

    Added Test Scripts

--- 3D/Assets/Scripts/GunScript.cs
+++ 3D/Assets/Scripts/GunScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
+public class GunScript : MonoBehaviour
 {
     private FirstPersonController _input;
     [SerializeField]
@@ -21,22 +21,66 @@ public class Gun : MonoBehaviour
     private float shellLifeTime = 2f;
     [SerializeField]
     private float fireRate = 0.5f;
+    [SerializeField]
+    private float aimOffset = 0.5f;
+    [SerializeField]
+    private float zoomOffset = 0.5f;
 
+    private Vector3 defaultPosition;
+    private bool isAiming;
+    private bool isZooming;
 
     // Start is called before the first frame update
     void Start()
     {
         _input = transform.root.GetComponent<FirstPersonController>();
-
+        defaultPosition = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_input.shoot) {
+        if (_input.shoot)
+        {
             Shoot();
             _input.shoot = false;
         }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            isAiming = true;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            isAiming = false;
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            isZooming = true;
+        }
+        else if (Input.GetMouseButtonUp(2))
+        {
+            isZooming = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (isAiming)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, defaultPosition + Vector3.forward * aimOffset, Time.deltaTime * 5f);
+        }
+        else if (isZooming)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, defaultPosition + Vector3.forward * zoomOffset, Time.deltaTime * 5f);
+        }
+        else
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, defaultPosition, Time.deltaTime * 5f);
+        }
+
+        transform.LookAt(Camera.main.transform.position + Camera.main.transform.forward * 100f, Camera.main.transform.up);
     }
 
     void Shoot()
