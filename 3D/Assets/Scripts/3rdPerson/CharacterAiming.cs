761070bab2daee7c1266a2e401429aac131705cb
monsterwhat <monster_what@hotmail.com>
2023-03-28 19:35:39 -0600

    Updates

--- 3D/Assets/Scripts/3rdPerson/CharacterAiming.cs
+++ 3D/Assets/Scripts/3rdPerson/CharacterAiming.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using UnityEngine.InputSystem;
 
 public class CharacterAiming : MonoBehaviour
 {
     public float turnSpeed = 20f;
+    public float aimDuration = 0.3f;
+
+    public Rig aimLayer;
     Camera Maincamera;
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         Maincamera = Camera.main;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -19,5 +28,18 @@ public class CharacterAiming : MonoBehaviour
     {
         float yawCamera = Maincamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, yawCamera, 0f), turnSpeed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        }
+        else
+        {
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
+        animator.updateMode = AnimatorUpdateMode.UnscaledTime;
     }
 }
--- 3D/Assets/prefabSpawner.cs
+++ 3D/Assets/prefabSpawner.cs
@@ -8,32 +8,63 @@ public class prefabSpawner : MonoBehaviour
     public GameObject prefab; // The prefab to spawn
     public TwoBoneIKConstraint RightHandikTarget;
     public TwoBoneIKConstraint LeftHandikTarget;
+    public MultiParentConstraint WeaponPose;
     private GameObject newObject; // The spawned prefab
+    private Animator animator;
 
     public void SpawnPrefab()
     {
-        // Instantiate the prefab as a child of this GameObject
+        // Spawn the prefab
         newObject = Instantiate(prefab, transform);
+        // Position it properly.
+        newObject.transform.position = transform.position;
+    }
+
+    void updateWeaponPose()
+    {
+        if (WeaponPose != null)
+        {
+            WeaponPose.data.constrainedObject = newObject.transform;
+        }
     }
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         SpawnPrefab();
     }
 
     void Update()
     {
+        if ( newObject != null )
+        {
+            animator.updateMode = AnimatorUpdateMode.UnscaledTime;
+            UpdateHands();
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UpdateHands();
+            LateUpdate();
+            updateWeaponPose();
+        }
 
     }
 
-    private void LateUpdate()
+    void UpdateHands()
     {
         // Update the IK target position
         var rightHand = newObject.transform.Find("RightHandPosition");
         RightHandikTarget.data.target = rightHand;
         var leftHand = newObject.transform.Find("LeftHandPosition");
         LeftHandikTarget.data.target = leftHand;
+
+    }
+
+    private void LateUpdate()
+    {
+        UpdateHands();
+        updateWeaponPose();
     }
 
 }
